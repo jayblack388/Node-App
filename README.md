74849ea09489e081ec7b619b3494921b985bb7c0
jayblack388 <jblackwell072588@gmail.com>
2018-01-11 20:05:13 -0500

    changed package name and re-init package lock

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Node-App
-Liri Homework
+(Homework) "Liri" Node.js App
+This is a Node application using the spotify, twitter, and request (resquesting OMDB's API) Node packages. It accepts four commands:
+		do-what-it-says
+		spotify-this-song
+		movie-this
+		my-tweets
+do-what-it-says
+spotify-this-song
+movie-this
+my-tweets
--- liri.js
+++ liri.js
@@ -1,35 +1,18 @@
 require("dotenv").config();
 let keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
 let fs = require("fs");
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
-let movieDB = new Omdb(keys.omdb);
+let movieKey = keys.omdb;
 let command = process.argv[2];
 let input = process.argv[3];
 
 //---------------------------------------------------
 
-switch (command) {
-	case "my-tweets":
-	  myTweets();
-	  break;
-	case "spotify-this-song":
-	  spotifyThisSong();
-	  break;
-	case "movie-this":
-	  movieThis();
-	  break;
-	case "do-what-it-says":
-	  doWhatItSays();
-	  break
-	default:
-	  console.log("Invalid Request");
-}
-
-//---------------------------------------------------
-
 const myTweets = () => {
-	console.log(client
+	console.log(process.argv
 		// Show my last 20 tweets and when they were created
 		)
 }
@@ -49,4 +32,23 @@ const doWhatItSays = () => {
 	console.log(
 		// use fs.readfile to use random.txt to call a command
 		)
+}
+
+//---------------------------------------------------
+
+switch (command) {
+	case "my-tweets":
+	  myTweets();
+	  break;
+	case "spotify-this-song":
+	  spotifyThisSong();
+	  break;
+	case "movie-this":
+	  movieThis();
+	  break;
+	case "do-what-it-says":
+	  doWhatItSays();
+	  break
+	default:
+	  console.log("Invalid Request");
 }
