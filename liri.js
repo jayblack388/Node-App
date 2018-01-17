79aaf22a742c4d47d6945bce2b788733349a667d
jayblack388 <jblackwell072588@gmail.com>
2018-01-17 11:09:37 -0500

    got twitter, omdb, and spotify functions working, random is WIP

--- liri.js
+++ liri.js
@@ -1,44 +1,101 @@
-require("dotenv").config();
-let keys = require("./keys.js");
-const Spotify = require('node-spotify-api');
-const Twitter = require('twitter');
-let fs = require("fs");
-let spotify = new Spotify(keys.spotify);
-let client = new Twitter(keys.twitter);
-let movieKey = keys.omdb;
-let command = process.argv[2];
-let input = process.argv[3];
+require("dotenv").config(); 
+const request = require("request"); 
+const keys = require("./keys.js"); 
+const Spotify = require('node-spotify-api'); 
+const Twitter = require('twitter'); 
+const fs = require("fs"); 
+const args = process.argv
+const spotify = new Spotify(keys.spotify); 
+const client = new Twitter(keys.twitter); 
+const movieKey = keys.omdb; 
+const command = args[2]; 
+const input = args[3]; 
+let inputString = ""; 
+let queryUrl; 
+let movieResponse; 
+let theSign;
 
-//---------------------------------------------------
+//--------------------------------------------------- //
+//Show last 20 tweets and when they were created, searches for user inputString 
+// If no tweet input, default to realDonaldTrump
+const tweets = () => {
+	if (inputString.length === 0) {
+		inputString = "@realDonaldTrump"
+	};
+	client.get('search/tweets', {q: inputString}, function(error, tweets, response) {
+		for (i = 0; i < 10; i++) {
+			console.log(`${i+1}) ` + tweets.statuses[i].created_at + `->` + tweets.statuses[i].text)
+		};    
+	});
+};
 
-const myTweets = () => {
-	console.log(process.argv
-		// Show my last 20 tweets and when they were created
-		)
-}
+// Display Artist(s), Song Name, Album, and Spotify Preview Link
+// If no song input, default to The Sign by Ace of Base  
 const spotifyThisSong = () => {
-	console.log(
-		// Display Artist(s), Song Name, Album, and Spotify Preview Link
-		// If no song input, default to The Sign by Ace of Base
-		)
-}
+	if (inputString.length === 0) {
+		inputString = "The Sign Ace of Base"
+	}
+		spotify.search({
+		type: 'track',
+		limit: 5,
+		query: inputString },
+		function(err, data) {
+			if (err) {
+				return console.log("Error occurred: " + err);
+			}
+			console.log("Top 5 Results")
+			console.log("----------------------------------------")
+
+			for (i = 0; i < 5; i++) {
+				console.log("Track Name: " + data.tracks.items[i].name);
+				console.log("Album Name: " + data.tracks.items[i].album.name);
+				console.log("Artist Name: " + data.tracks.items[i].artists[0].name);
+				console.log("Spotify Link: " + data.tracks.items[i].external_urls.spotify);
+				console.log("----------------------------------------")
+			}			
+		})
+};
+
+// Display title, release year, IMDB rating, Rotten Tomatoes rating, Country of production, Language, Plot, Actors in the movie        
+// If no movie input, default to 'Mr. Nobody'. 
 const movieThis = () => {
-	console.log(
-		// Display title, release year, IMDB rating, Rotten Tomatoes rating, Country of production, Language, Plot, Actors in the movie
-		// If no movie input, default to 'Mr. Nobody'.
-		)
+	queryUrl = "http://www.omdbapi.com/?apikey=trilogy&y=&plot=short&t=";
+	if (inputString.length === 0) {
+		queryUrl = queryUrl + "Mr+Nobody";
+	}else{
+		queryUrl = queryUrl + inputString;
+	};	
+		request(queryUrl, (error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				movieResponse = JSON.parse(body);
+				console.log(
+					"Title: " + movieResponse.Title + " | " + " Year Released: " + movieResponse.Year + " | " + " Language: " 
+					+ movieResponse.Language + " | " + " Produced in: " + movieResponse.Country + " | " + " Cast: " + 
+					movieResponse.Actors + " | " + " Plot: " + movieResponse.Plot + " | " + " Ratings: IMDB: " + 
+					movieResponse.Ratings[0].Value + " | " + " Rotten Tomatoes: " + movieResponse.Ratings[1].Value);
+			};
+		});
 }
+
+// use fs.readfile to use random.txt to call a command
 const doWhatItSays = () => {
 	console.log(
-		// use fs.readfile to use random.txt to call a command
-		)
-}
+
+	)
+};
 
 //---------------------------------------------------
+for (let i = 3; i < args.length; i++) {
+	if (i > 3 && i < args.length) {
+		inputString = inputString + "+" + args[i];
+	}else {
+		inputString += args[i];
+	}    
+};
 
 switch (command) {
-	case "my-tweets":
-	  myTweets();
+	case "tweets":
+	  tweets();
 	  break;
 	case "spotify-this-song":
 	  spotifyThisSong();
