9f9e3dbeb7f72d6fcb9b52025516085e7f306edc
jayblack388 <jblackwell072588@gmail.com>
2018-01-18 17:42:03 -0500

    made choices more semantic

--- liri.js
+++ liri.js
@@ -164,25 +164,25 @@ const runProg = () => {
 				type: "list",
 				name: "command",
 				message: "What command do you want to use?",
-				choices: ["tweets", "spotify-this-song", "movie-this", "do-what-it-says", "add-command"]
+				choices: ["tweets=> search twitter for users/topics", "spotify-this-song=> search spotify for songs", "movie-this=> search imdb for movie information", "do-what-it-says=> run a random command/search", "add-command=> add a command/search to the do-what-it-says list"]
 			},
 	]).then((input) => {
 		switch (input.command) {
-			case "tweets":
-				askQuery(tweets)
+			case "tweets=> search twitter for users/topics":
+			  askQuery(tweets)
 			  break;
-			case "spotify-this-song":
+			case "spotify-this-song=> search spotify for songs":
 			  askQuery(spotifyThisSong);
 			  break;
-			case "movie-this":
+			case "movie-this=> search imdb for movie information":
 			  askQuery(movieThis);
 			  break;
-			case "do-what-it-says":
+			case "do-what-it-says=> run a random command/search":
 			  doWhatItSays();
 			  break;
-			case "add-command":
-				addRandom();
-				break
+			case "add-command=> add a command/search to the do-what-it-says list":
+			  addRandom();
+			  break
 			default:
 			  console.log("Invalid Request");
 		};
